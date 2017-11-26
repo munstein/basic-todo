6118f0c9a04d1a828db08adadcf3c92d8de95d80
munstein <eduardo.munstein@live.com>
2017-11-25 23:34:07 -0200

    Added Base Package

---
+++ app/src/main/java/com/munstein/basictodokotlin/base/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.munstein.basictodokotlin.base
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by @Munstein on 25/11/2017. --23:31
+ */
+open class BaseActivity : AppCompatActivity() {
+}
--- app/src/main/java/com/munstein/basictodokotlin/main/MainActivity.kt
+++ app/src/main/java/com/munstein/basictodokotlin/main/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.munstein.basictodokotlin.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.munstein.basictodokotlin.R
-import com.munstein.basictodokotlin.data.DataAccess
+import com.munstein.basictodokotlin.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var db = DataAccess()
-        db.test()
-
     }
 }
