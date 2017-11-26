64dbf7389ba38f3c4eed01993a68baf2972f00a8
munstein <eduardo.munstein@live.com>
2017-11-25 23:36:35 -0200

    Added feature to BaseActivity.kt

--- app/src/main/java/com/munstein/basictodokotlin/base/BaseActivity.kt
+++ app/src/main/java/com/munstein/basictodokotlin/base/BaseActivity.kt
@@ -1,9 +1,15 @@
 package com.munstein.basictodokotlin.base
 
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 /**
  * Created by @Munstein on 25/11/2017. --23:31
  */
 open class BaseActivity : AppCompatActivity() {
+
+    fun showToast(msg : String){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
 }
