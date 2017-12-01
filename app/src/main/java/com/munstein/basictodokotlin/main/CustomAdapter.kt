b0565abcfeaa5d765f1b3dacf2c8c75fe354ae45
munstein <eduardo.munstein@live.com>
2017-12-01 16:00:22 -0200

    Adapter Commit

---
+++ app/src/main/java/com/munstein/basictodokotlin/main/CustomAdapter.kt
@@ -0,0 +1,46 @@
+package com.munstein.basictodokotlin.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import android.widget.ToggleButton
+import com.munstein.basictodokotlin.R
+import com.munstein.basictodokotlin.model.Task
+
+/**
+ * Created by @Munstein on 01/12/2017. --15:06
+ */
+class CustomAdapter(context : Context, tasks : List<Task>) : BaseAdapter() {
+
+    private val inflater = LayoutInflater.from(context)
+    private var tasks : List<Task> = tasks
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view = this.inflater.inflate(R.layout.custom_list_item, parent, false)
+        var task = tasks.get(position)
+
+        var textDescription = view.findViewById<TextView>(R.id.custom_text_description)
+        var toggleFavorite = view.findViewById<ToggleButton>(R.id.custom_toggle_favorite)
+
+        textDescription.text = task.description
+        toggleFavorite.isPressed = task.isFavorite
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return tasks.get(position)
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return 0;
+    }
+
+    override fun getCount(): Int {
+        return tasks.size
+    }
+
+}
--- app/src/main/java/com/munstein/basictodokotlin/main/MainActivity.kt
+++ app/src/main/java/com/munstein/basictodokotlin/main/MainActivity.kt
@@ -3,16 +3,29 @@ package com.munstein.basictodokotlin.main
 import android.os.Bundle
 import com.munstein.basictodokotlin.R
 import com.munstein.basictodokotlin.base.BaseActivity
+import com.munstein.basictodokotlin.model.Task
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(), MainMVP.view {
+
+    private lateinit var adapter : CustomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var task = Task("Fine", true)
+        var taskList = ArrayList<Task>()
+        taskList.add(task)
+
+        var adapter = CustomAdapter(this, taskList)
+
+        main_listview.adapter = adapter
+
     }
 
     override fun updateView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun showMessage(msg: String) {
